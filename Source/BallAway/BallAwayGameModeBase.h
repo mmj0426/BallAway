a389a6eb78e5828584a210803e732e529293a89f
mmj0426 <ehskfemejr3@gmail.com>
2021-07-14 17:15:13 +0900

    Create Project

---
+++ Source/BallAway/BallAwayGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BallAwayGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLAWAY_API ABallAwayGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
